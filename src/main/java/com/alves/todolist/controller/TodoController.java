d8e0e3d2e4dd9d0ee4c518b691b464f8ac77ced4
Raquel Brito <raquelbrito662@gmail.com>
2023-10-22 23:00:19 -0300

    criacao do rest controller

---
+++ src/main/java/com/alves/todolist/controller/TodoController.java
@@ -0,0 +1,41 @@
+package com.alves.todolist.controller;
+
+import com.alves.todolist.entity.Todo;
+import com.alves.todolist.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<List<Todo>> ceate(@RequestBody Todo todo) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.create(todo));
+    }
+
+    @GetMapping
+    public List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Todo> delete(@PathVariable Long id) {
+        return todoService.delete(id);
+    }
+}
