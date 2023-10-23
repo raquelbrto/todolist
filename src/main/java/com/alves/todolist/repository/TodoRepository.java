8e3e3eef42c0333775b5c82b6685ea9a959eeec7
Raquel Brito <raquelbrito662@gmail.com>
2023-10-22 22:42:44 -0300

    implementando service, entity e repository da tarefa

---
+++ src/main/java/com/alves/todolist/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.alves.todolist.repository;
+
+import com.alves.todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
